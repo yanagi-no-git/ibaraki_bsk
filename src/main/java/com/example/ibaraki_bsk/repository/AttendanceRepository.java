ed5ce079c2b95fa308c2cbee493a6ec263845061
高柳　亮 <ryo_takayanagi@takayanagiryounoMacBook-Air.local>
2026-03-01 23:32:57 +0900

    tailwindcssの導入

--- src/main/java/com/example/ibaraki_bsk/repository/AttendanceRepository.java
+++ src/main/java/com/example/ibaraki_bsk/repository/AttendanceRepository.java
@@ -2,8 +2,9 @@ package com.example.ibaraki_bsk.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.ibaraki_bsk.entity.AttendanceId;
 import com.example.ibaraki_bsk.entity.Attendance;
 
-public interface AttendanceRepository extends CrudRepository<Attendance, Integer>{
+public interface AttendanceRepository extends CrudRepository<Attendance, AttendanceId>{
     
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** tailwindの設定ファイル */ 
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ["src/main/resources/templates/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
